fd30f131aa467591f6ab762d8f04316ec98175f1
AlanSanchezC <acazarez58@gmail.com>
2017-04-21 13:31:08 -0500

    Inventario por lista enlazada ordenada

---
+++ ControlDeInventario/Inventario.cs
@@ -0,0 +1,100 @@
+﻿/*
+ *  Creado por Alan Francisco Sánchez Cazarez
+ */
+
+using System;
+
+namespace ControlDeInventario
+{
+    class Inventario
+    {
+        private Producto inicio;
+
+        public Inventario()
+        {
+            inicio = null;
+        }
+
+        public bool agregar(Producto p)
+        {
+            Producto pr = inicio;
+
+            if (inicio == null)
+            {
+                inicio = p;
+            }
+            else
+            { 
+                if (p.nombre.CompareTo(pr.nombre) < 0)
+                {
+                    p.siguiente = pr;
+                    inicio = p;
+                }
+                else 
+                {
+                    try
+                    {
+                        while (p.nombre.CompareTo(pr.siguiente.nombre) > 0)
+                        {
+                            pr = pr.siguiente;
+                        }
+                        p.siguiente = pr.siguiente;
+                        pr.siguiente = p;
+                    }
+                    catch (System.NullReferenceException)
+                    {
+                        pr.siguiente = p;
+                    }
+                }
+             }
+            return true;
+        }
+
+        public string reporte()
+        {
+            string cad = "";
+            Producto pr = inicio;
+
+            while(pr != null)
+            {
+                cad += pr.ToString() + Environment.NewLine;
+                pr = pr.siguiente;
+            }
+
+            return cad;
+        }
+
+        public Producto busqueda(string codigo)
+        {
+            Producto pr = inicio;
+
+            while(pr.codigo != codigo)
+            {
+                pr = pr.siguiente;
+            }
+            return pr;
+        }
+
+        public void eliminar(string codigo)
+        {
+            Producto pr = inicio;
+            bool encontrado = false;
+
+            if (inicio.codigo == codigo)
+            {
+                inicio = inicio.siguiente;
+                encontrado = true;
+            }
+
+            while (!encontrado)
+            {
+                if (pr.siguiente.codigo == codigo)
+                {
+                    pr.siguiente = pr.siguiente.siguiente;
+                    encontrado = true;
+                }
+                pr = pr.siguiente;
+            }
+        }
+    }
+}
